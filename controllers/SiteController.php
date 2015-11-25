46ffa48e4fcb8595dc656cdaf4848ce81782b4a4
Ilya Gorokhov <tairesh.rus@gmail.com>
2015-11-25 16:24:29 +0500

    Pagination & sorting

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,7 +2,8 @@
 
 namespace app\controllers;
 
-use yii\web\Controller,
+use Yii,
+    yii\web\Controller,
     yii\data\SqlDataProvider;
 
 class SiteController extends Controller
@@ -19,6 +20,7 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
+        $count = Yii::$app->db->createCommand('SELECT COUNT(*) FROM books')->queryScalar();
         $dataProvider = new SqlDataProvider([
             'sql' => 'SELECT books.*,
                         authors.first_name || " " || authors.last_name author,
@@ -28,10 +30,16 @@ class SiteController extends Controller
                         ON authors.id = books.author_id
                     LEFT JOIN publishers 
                         ON publishers.id = books.publisher_id',
+            'totalCount' => $count,
             'pagination' => [
                 'pageSize' => 5,
             ],
+            'sort' => [
+                'defaultOrder' => ['author' => SORT_ASC],
+                'attributes' => ['name', 'author', 'publisher', 'price', 'year']
+            ]
         ]);
         return $this->render('index', ['dataProvider' => $dataProvider]);
     }
+    
 }
